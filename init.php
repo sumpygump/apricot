1330a12f5f6fd5c16514b568378b5571c9b16912
Jansen Price <sumpygump@gmail.com>
2022-12-17 22:47:02 -0600

    Correct order with session_start

--- init.php
+++ init.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Initialize environment
  *
@@ -7,7 +8,6 @@
 
 define('APP_ROOT', realpath(dirname(__FILE__)));
 
-session_start();
 date_default_timezone_set('America/Chicago');
 
 ini_set('magic_quotes_gpc', 0);
@@ -15,12 +15,14 @@ ini_set('expose_php', 0);
 ini_set('always_populate_raw_post_data', 0);
 ini_set('session.gc_divisor', '1000');
 
+session_start();
+
 // Include path
-$paths = array(
+$paths = [
     get_include_path(),
     APP_ROOT,
     APP_ROOT . "/lib",
-);
+];
 set_include_path(implode(PATH_SEPARATOR, $paths));
 
 // Register autoloader
